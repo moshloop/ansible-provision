f0040537c824c2073e6b0e96199a32f1c1960b17
Moshe Immerman <moshe.immerman@vitalitygroup.com>
2018-10-18 10:50:17 +0200

    test fixes

--- tests/test.sh
+++ tests/test.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+AWS_REGION=eu-west-1
+
 function finish {
   echo "Cleaning up stack"
   aws cloudformation delete-stack --stack-name $stack --region $AWS_REGION
 }
 trap finish EXIT
 
-AWS_REGION=eu-west-1
+function get_ip() {
+  aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --filters "Name=tag:aws:cloudformation:stack-name,Values=$1" --region $AWS_REGION | jq -r '.Reservations[].Instances[0].PublicIpAddress'
+}
+
+function is_cloudinit_finished() {
+    ssh -o StrictHostKeyChecking=no ec2-user@$IP "ls /var/lib/cloud/instance/boot-finished" 2>&1 > /dev/null
+}
+
+start=$(date +%s)
 test=$1
 shift
 stack=test$(date +"%Y%m%dT%H%M%S")
-echo $stack
+echo "Cloudformation stack name: $stack"
+ansible-playbook --version
 cmd="ansible-playbook -i tests/$test play.yml -c local -e stack_name=$stack \
  -e account_id=$(aws sts get-caller-identity | jq -r '.Account') \
  -e region=$AWS_REGION\
@@ -22,15 +33,16 @@ cmd="ansible-playbook -i tests/$test play.yml -c local -e stack_name=$stack \
 echo $cmd
 $cmd $@
 
-IP=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --filters "Name=tag:aws:cloudformation:stack-name,Values=$stack" --region $AWS_REGION | jq -r '.Reservations[].Instances[0].PublicIpAddress')
-echo "Provisioned $IP, waiting 2m for cloud-init"
+IP=$(get_ip $stack)
+echo "Provisioned $IP, waiting for up to 2m"
 
-while ! ssh -o StrictHostKeyChecking=no ec2-user@$IP "ls /var/lib/cloud/instance/boot-finished" ; do
+while is_cloudinit_finished $IP || [[ $((now - start)) -lt "120" ]] ; do
   echo .
   sleep 5
+  now=$(date +%s)
 done
 
-if [[ -e tests/$test.rb ]]; then
+if [[ -e tests/$test.rb && is_cloudinit_finished $IP ]]; then
   if ! inspec exec tests/$test.rb -t ssh://ec2-user@$IP; then
       ssh ec2-user@$IP
   fi
