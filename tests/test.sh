93ab436d20b094a9609631eb67eeba3eeec5326e
Moshe Immerman <moshe.immerman@vitalitygroup.com>
2018-10-19 15:37:33 +0200

    fix tests

--- tests/test.sh
+++ tests/test.sh
@@ -13,18 +13,21 @@ function get_ip() {
 }
 
 function is_cloudinit_finished() {
-    ssh -o StrictHostKeyChecking=no ec2-user@$IP "ls /var/lib/cloud/instance/boot-finished" 2>&1 > /dev/null
+  echo "Checking"
+    ssh -o StrictHostKeyChecking=no ec2-user@$1 "ls /var/lib/cloud/instance/boot-finished" 2>&1 > /dev/null
 }
 
-if [[ ! -e ~/.ssh/.id_rsa ]]; then
-  echo "No SSH found, generating"
-  ssh-keygen -f ~/.ssh/id_rsa -N ""
+KEY=$HOME/.ssh/id_rsa
+test -e $KEY
+if [[ ! -e "$KEY" ]]; then
+  echo "No SSH Key found, generating"
+  ssh-keygen -f $KEY -N ""
 fi
 
 if [[ "$SSH_AUTH_SOCK" == "" ]]; then
   echo "Starting ssh-agent"
   eval $(ssh-agent -s)
-  ssh-add ~/.ssh/.id_rsa
+  ssh-add $KEY
 fi
 
 start=$(date +%s)
@@ -46,14 +49,15 @@ $cmd $@
 IP=$(get_ip $stack)
 echo "Provisioned $IP, waiting for up to 2m"
 
-while is_cloudinit_finished $IP || [[ $((now - start)) -lt "120" ]] ; do
+while ! is_cloudinit_finished $IP && [[ $((now - start)) -lt "120" ]] ; do
   echo .
   sleep 5
   now=$(date +%s)
 done
 
-if [[ -e tests/$test.rb && is_cloudinit_finished $IP ]]; then
-  if ! inspec exec tests/$test.rb -t ssh://ec2-user@$IP; then
+if [[ -e "tests/$test.rb"  ]] && is_cloudinit_finished $IP; then
+  echo "Executing tests/$test.rb"
+  if ! inspec exec tests/$test.rb -t ssh://ec2-user@$IP && [[ "$-" == "i" ]]; then
       ssh ec2-user@$IP
   fi
 fi
