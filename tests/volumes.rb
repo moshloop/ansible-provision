694859e33b79eb1c5cd522e4a9c448f05b0bda9a
Moshe Immerman <moshe.immerman@vitalitygroup.com>
2018-10-20 20:31:49 +0200

    fix test

--- tests/volumes.rb
+++ tests/volumes.rb
@@ -1,5 +1,5 @@
-['/mnts/d1', '/mnts/d2', '/mnts/d3', '/mnts/d4', '/mnt/nvm'].each do |m|
+['/mnts/d1', '/mnts/d2', '/mnts/d3', '/mnts/d4'].each do |m|
     describe mount(m) do
-    it { should be_mounted }
+      it { should be_mounted }
     end
 end
