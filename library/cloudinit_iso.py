c309bbc823dddd59c49b29d7726205e0e039e5a4
Moshe Immerman <moshe.immerman@vitalitygroup.com>
2018-11-12 10:16:05 +0200

    cloudinit support for coreos

--- library/cloudinit_iso.py
+++ library/cloudinit_iso.py
@@ -5,7 +5,7 @@ author:
     - "Moshe Immerman"
 short_description:  Creates an ISO image containing cloud-init user-data files
 description:
-    -  Creates systemd services
+    -  Creates an ISO image containing cloud-init user-data files
 options:
     dest:
         required: True
@@ -13,6 +13,10 @@ options:
     user:
         required: True
         description: ['']
+    type:
+        required: False
+        default: vmware
+        description: The type of cloud-init ISO to create, Valid values are: vmware,coreos
     meta:
         required: True
         description: ['']
@@ -20,29 +24,25 @@ options:
 EXAMPLES = '''
      - cloudinit_iso:
           dest: /tmp/cloudinit.iso
-          user1: |
+          type: vmware
+          user: |
             "
 '''
 import os.path
 from subprocess import check_call
 from ansible.module_utils.basic import AnsibleModule
-import logging
-
-logger = logging.getLogger('cloudinit-iso')
-hdlr = logging.FileHandler('/tmp/ansible-cloudinit-iso.log')
-logger.addHandler(hdlr)
-logger.setLevel(logging.DEBUG)
-
-
-import os
+import os, sys
 from tempfile import gettempdir
 
+def log(s):
+    sys.stderr.write("%s\n" % s)
 
 def main():
     arg_spec = dict(
         dest=dict(required=True),
         user=dict(required=True),
         meta=dict(required=False),
+        type=dict(required=False,default="vmware")
     )
     module = AnsibleModule(
         argument_spec=arg_spec,
@@ -50,10 +50,15 @@ def main():
     )
 
     tmp = os.path.join(gettempdir(), '{}'.format(hash(os.times())))
-    logger.info("Writing temp user-data.txt: " + tmp)
+    log("Writing temp user-data.txt: " + tmp)
     os.makedirs(tmp)
 
-    with open(tmp + "/user-data", 'w') as f:
+    path = "/user-data"
+    if module.params['type'] == 'coreos':
+        os.makedirs(tmp + "/openstack/latest")
+        path = "/openstack/latest/user_data"
+
+    with open(tmp + path, 'w') as f:
         f.write(module.params['user'])
     with open(tmp + "/meta-data", "w") as f:
         f.write('instance-id: iid-123456\n')
@@ -63,7 +68,10 @@ def main():
         else:
             f.write('local-hostname: cloudy')
 
-    check_call("genisoimage -output %s -volid cidata -joliet -rock user-data meta-data" % module.params['dest'], shell=True,cwd=tmp)
+    if module.params['type'] == 'coreos':
+       check_call("genisoimage -output %s -volid config-2 -joliet -rock ." % module.params['dest'], shell=True,cwd=tmp)
+    else:
+        check_call("genisoimage -output %s -volid cidata -joliet -rock user-data meta-data" % module.params['dest'], shell=True,cwd=tmp)
 
     module.exit_json(changed=True)
 
